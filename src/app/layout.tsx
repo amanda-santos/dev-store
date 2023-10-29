046e56825dc9b612463c2907606345c1759a225c
amanda-santos <amandasf0200@gmail.com>
2023-10-29 20:08:42 -0300

    feat: create Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html className={inter.variable} lang="pt">
-      <body>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">
+        {children}
+      </body>
     </html>
   )
 }
