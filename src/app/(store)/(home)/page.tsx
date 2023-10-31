74ac958db1e56edfc3e3adee8f731a1c2d8f6716
amanda-santos <amandasf0200@gmail.com>
2023-10-31 20:11:18 -0300

    feat: customize page titles with Metadata

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,9 @@
-import { api } from '@/data/api'
-import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api('/products/featured', {
@@ -13,6 +15,10 @@ async function getFeaturedProducts(): Promise<Product[]> {
   const products = await response.json()
 
   return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
 }
 
 export default async function Home() {
