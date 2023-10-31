a210918d4677edc24bd8da8bbc72ed61f269edb5
amanda-santos <amandasf0200@gmail.com>
2023-10-31 20:08:32 -0300

    feat: create product API route and use it on page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -39,8 +39,6 @@ export default async function Home() {
             {highlightedProduct.price.toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
-              minimumFractionDigits: 0,
-              maximumFractionDigits: 0,
             })}
           </span>
         </div>
