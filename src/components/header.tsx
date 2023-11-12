72d6cf7e7e416d3733358a0f81329e7524105957
amanda-santos <amandasf0200@gmail.com>
2023-11-12 18:18:31 -0300

    test: 🧪 add E2E tests for adding a product to the cart

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
         <div className="w-px h-4 bg-zinc-700"></div>
 
         <Link href="/" className="flex items-center gap-2 hover:underline">
-          <span className="text-sm">Account</span>
+          <span className="text-sm">Minha conta</span>
           <Image
             src="https://github.com/amanda-santos.png"
             className="h-6 w-6 rounded-full"
